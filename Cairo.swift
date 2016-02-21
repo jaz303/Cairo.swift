3f8a372a926c2296ac2e304c04edc47648333ecd
Jason Frame <jason@onehackoranother.com>
2016-02-21 14:29:40 +0000

    matrix improvements

--- Cairo.swift
+++ Cairo.swift
@@ -1,6 +1,59 @@
 import Ccairo
 
 public typealias Matrix = cairo_matrix_t
+extension Matrix {
+	public static func zero() -> Matrix {
+		return Matrix(xx: 0, yx: 0,
+		              xy: 0, yy: 0,
+		              x0: 0, y0: 0)
+	}
+
+	public static func identity() -> Matrix {
+		return Matrix(xx: 1, yx: 0,
+		              xy: 0, yy: 1,
+		              x0: 0, y0: 0)
+	}
+}
+
+public func *(l: Matrix, r: Double) -> Matrix {
+	return Matrix(xx: l.xx * r,
+				  yx: l.yx * r,
+				  xy: l.xy * r,
+				  yy: l.yy * r,
+				  x0: l.x0 * r,
+				  y0: l.y0 * r)
+}
+
+public func *(l: Matrix, r: Matrix) -> Matrix {
+	return Matrix.zero()
+}
+
+public func /(l: Matrix, r: Double) -> Matrix {
+	return Matrix(xx: l.xx / r,
+				  yx: l.yx / r,
+				  xy: l.xy / r,
+				  yy: l.yy / r,
+				  x0: l.x0 / r,
+				  y0: l.y0 / r)
+}
+
+public func +(l: Matrix, r: Matrix) -> Matrix {
+	return Matrix(xx: l.xx + r.xx,
+				  yx: l.yx + r.yx,
+				  xy: l.xy + r.xy,
+				  yy: l.yy + r.yy,
+				  x0: l.x0 + r.x0,
+				  y0: l.y0 + r.y0)
+}
+
+public func -(l: Matrix, r: Matrix) -> Matrix {
+	return Matrix(xx: l.xx - r.xx,
+				  yx: l.yx - r.yx,
+				  xy: l.xy - r.xy,
+				  yy: l.yy - r.yy,
+				  x0: l.x0 - r.x0,
+				  y0: l.y0 - r.y0)
+}
 
 public class ImageSurface {
 	public init(width: Int, height: Int) {
@@ -44,10 +97,12 @@ public class Context {
 	//
 	// Transformations
 
-	public func translate(dx: Double, dy: Double) { cairo_translate(ctx, dx, dy) }
-	public func scale(sx: Double, sy: Double) { cairo_translate(ctx, sx, sy) }
+	public func translate(dx dx: Double, dy: Double) { cairo_translate(ctx, dx, dy) }
+	public func scale(factor: Double) { cairo_translate(ctx, factor, factor); }
+	public func scale(sx sx: Double, sy: Double) { cairo_translate(ctx, sx, sy) }
 	public func rotate(angle: Double) { cairo_rotate(ctx, angle) }
 	public func transform(inout matrix: Matrix) { cairo_transform(ctx, &matrix) }
+	public func getMatrix(inout matrix: Matrix) { cairo_get_matrix(ctx, &matrix) }
 	public func setMatrix(inout matrix: Matrix) { cairo_set_matrix(ctx, &matrix) }
 	public func copyMatrixTo(inout matrix: Matrix) { cairo_get_matrix(ctx, &matrix) }
 	public func setIdentity() { cairo_identity_matrix(ctx) }
